81b8a905019b2de2f7b57d79d6ec31572fd3e6aa
HtetAungLinn98 <92046483+htetaunglinn1998@users.noreply.github.com>
2023-07-31 10:37:02 +0630

    feat: dynamic cards added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,71 @@
+const bestSellers = [
+  {
+    id: 1,
+    img: './assets/stocks/5.png',
+    title: 'Ultra Hiking Boots',
+    description: 'Easy to Dry. Able to carry around. Sole can replaceable.',
+    price: 249,
+    discount: 385
+  },
+  {
+    id: 2,
+    img: './assets/stocks/2.png',
+    title: 'Cooking double set',
+    description: 'Handdle can foldable. Can carry with backpack.',
+    price: 78,
+    discount: 119
+  },
+  {
+    id: 3,
+    img: './assets/stocks/3.png',
+    title: 'Camping backpack',
+    description: 'One of the lightest backpacks for camping. Water proof.',
+    price: 139,
+    discount: 179
+  },
+  {
+    id: 4,
+    img: './assets/stocks/4.png',
+    title: 'Single Sleeping Bag',
+    description: 'Able to carry around. Light weight. Water proof.',
+    price: 129,
+    discount: 149
+  },
+  {
+    id: 5,
+    img: './assets/stocks/1.png',
+    title: 'Single Camping Tent',
+    description: 'Double sheet covered. Wind Proof.',
+    price: 249,
+    discount: 396
+  },
+  {
+    id: 6,
+    img: './assets/stocks/6.png',
+    title: 'Classical Lamp',
+    description: 'Good for who wants vintage vibe while camping.',
+    price: 99,
+    discount: 125
+  },
+  {
+    id: 7,
+    img: './assets/stocks/7.png',
+    title: 'Magnetic Compass',
+    description: 'Able to carry around and water proof.',
+    price: 159,
+    discount: 239
+  },
+  {
+    id: 8,
+    img: './assets/stocks/8.png',
+    title: 'Medium Battery Touch',
+    description: 'Only 40 minutes to full charge. Powerful touch.',
+    price: 47,
+    discount: 78
+  }
+]
+
+// Humbarger Menu Button
 function toggleMenu () {
   // Side Menu Show
   const menuBtn = document.querySelector('.menu__btn')
@@ -6,11 +74,11 @@ function toggleMenu () {
   menuBtn.classList.toggle('menu__transparent')
 }
 
+// App scope start here
 document.addEventListener('DOMContentLoaded', () => {
   // Function to hide the loading screen
   function hideLoadingScreen () {
     const loadingScreen = document.getElementById('loader__container')
-    console.log(loadingScreen)
     loadingScreen.style.display = 'none'
   }
 
@@ -77,6 +145,30 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // Cloning Brand slide Node
-  var cloneSlide = document.querySelector('.logo__slide').cloneNode(true)
-  document.querySelector('.brand__slider__section').appendChild(cloneSlide)
+  var cloneSlide = document.querySelector('.logo__slide')?.cloneNode(true)
+  document.querySelector('.brand__slider__section')?.appendChild(cloneSlide)
+
+  // Add to equipment section
+  let equipmentGrid = document.getElementById('equipmentGrid')
+
+  function bestSeller () {
+    equipmentGrid.innerHTML = bestSellers.map(item => {
+      const { id, img, title, description, price, discount } = item
+      return(
+        `<div class="grid__item">
+        <img src=${img} alt="tent" class="stock__img">
+        <a href="#" class="stock__title">${title}</a>
+        <p class="review">${description}</p>
+        <p class="price"><span>$${price}</span><del>$${discount}</del></p>
+        <hr>
+        <div class="shop__container stock__buttons">
+          <button onclick="addToCart(this);"><i class="fa-solid fa-cart-plus"></i>Shop Now</button>
+          <p>Free Shipping</p>
+        </div>
+      </div>`
+      )
+    }).join('')
+  }
+
+  bestSeller()
 })
